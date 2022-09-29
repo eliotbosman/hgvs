bf148732eddef8b61972158ccc3e25c7e3e88d11
eliotbosman <114088053+eliotbosman@users.noreply.github.com>
2022-09-29 13:51:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ var vid = document.getElementById("bgvideo");
 function setPlaySpeed() { 
     vid.playbackRate = 0.1;
   } 
+
+var pop = document.getElementsByClassName("popup")[0];
+
+function overlayw() {
+    popup.style.display = "grid";
+    popup.style.transition = "all 2s";
+}
